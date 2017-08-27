62bfc64f12c9cd56ba3d653a9bf17dfdaa30103c
zhuangyan <zy_php@163.com>
2017-08-27 09:17:28 +0800

    apipath

--- fullstack-course5/assignment5-solution/src/common/common.module.js
+++ fullstack-course5/assignment5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://davids-restaurant.herokuapp.com')
+.constant('ApiPath', 'https://aleksandar-gjorgievski.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];

ffb6e3e9c646e1d93cade5d30412f37044ae02ff
zhuangyan <zy_php@163.com>
2017-09-04 16:03:25 +0800

    a

--- fullstack-course5/assignment5-solution/src/common/common.module.js
+++ fullstack-course5/assignment5-solution/src/common/common.module.js
@@ -2,11 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-<<<<<<< HEAD
 .constant('ApiPath', 'https://luiz-angular-coursera.herokuapp.com')
-=======
-.constant('ApiPath', 'https://aleksandar-gjorgievski.herokuapp.com')
->>>>>>> 62bfc64f12c9cd56ba3d653a9bf17dfdaa30103c
 .config(config);
 
 config.$inject = ['$httpProvider'];

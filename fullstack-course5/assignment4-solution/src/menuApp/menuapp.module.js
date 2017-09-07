6720de4f8d956f73a267a8218712e937432e24f9
zhuangyan <zy_php@163.com>
2017-09-07 16:41:52 +0800

    del

--- fullstack-course5/assignment4-solution/src/menuApp/menuapp.module.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict';
-
-  angular.module('MenuApp', ['data', 'ui.router']);
-
-
-})();
--- fullstack-course5/assignment5-solution/src/public/public.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-"use strict";
-/**
- * Public restaurant application. Includes the common module and ui-router.
- */
-angular.module('public', ['ui.router', 'common']);
-
-})();
